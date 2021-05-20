a3bea11b65e1c7c449b0154c0bcc01d1c110fa4d
rodrigo-represa <rodrigo.represa@mediamonks.com>
2021-05-20 16:34:38 +0200

    Update Jetpack Compose to beta07 and add accompanist horizontal pager

--- app/src/main/java/com/represa/draw/MainActivity.kt
+++ app/src/main/java/com/represa/draw/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -13,21 +12,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.LocalSystemUiController
 import com.google.accompanist.systemuicontroller.rememberAndroidSystemUiController
-import com.represa.draw.ui.theme.DrawTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             // Remember a controller, and provide it to the LocalSystemUiController
-            val controller = rememberAndroidSystemUiController()
-            CompositionLocalProvider(LocalSystemUiController provides controller) {
+            //val controller = rememberAndroidSystemUiController()
+            //CompositionLocalProvider(LocalSystemUiController provides controller) {
                 Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
                     updateStatusBar()
-                    PhysicoChest()
+                    //PhysicoChest()
+                    DotsIndicator()
                 }
             }
-        }
+        //}
     }
 
     @Composable
