d8dc26505dddfb21673dfdd77d823348a0cfaff1
rodrigo-represa <rodrigo.represa@mediamonks.com>
2021-05-31 12:50:13 +0200

    Show status bar transparent again

--- app/src/main/java/com/represa/draw/MainActivity.kt
+++ app/src/main/java/com/represa/draw/MainActivity.kt
@@ -4,44 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.systemuicontroller.LocalSystemUiController
-import com.google.accompanist.systemuicontroller.rememberAndroidSystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Remember a controller, and provide it to the LocalSystemUiController
-            //val controller = rememberAndroidSystemUiController()
-            //CompositionLocalProvider(LocalSystemUiController provides controller) {
                 Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
                     updateStatusBar()
                     //PhysicoChest()
                     DotsIndicator()
                 }
-            }
-        //}
+
+        }
     }
 
     @Composable
-    private fun updateStatusBar() {
-        // Get the current SystemUiController
-        val systemUiController = LocalSystemUiController.current
+    fun updateStatusBar() {
+        // Remember a SystemUiController
+        val systemUiController = rememberSystemUiController()
+        val useDarkIcons = !MaterialTheme.colors.isLight
 
         SideEffect {
             // Update all of the system bar colors to be transparent, and use
             // dark icons if we're in light theme
             systemUiController.setSystemBarsColor(
                 color = Color.Transparent,
-                darkIcons = false
+                darkIcons = useDarkIcons
             )
 
             // setStatusBarsColor() and setNavigationBarsColor() also exist
